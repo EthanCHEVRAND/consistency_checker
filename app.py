a9fe91cdb7e63a72292a5d745b378962ba64284f
EthanCHEVRAND <ethan.chevrand@edu.univ-fcomte.fr>
2025-02-11 17:59:01 +0100

    printing values in a tkinter window

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from tkinter import *
+from tkinter import ttk
 from time import time_ns
 
 last_click = time_ns()
-print(last_click)
 
 def key_press(event):
     global last_click
@@ -17,10 +17,16 @@ def show_time(time):
     if time >= 1000:
         unit = "s"
         time /= 1000
+    time=round(time, 2)
+    result.config(text=(time, unit))
     print(time, unit)
 
 
 root = Tk()
+
 root.geometry('640x480')
+result=Label(root, height=5, width=20, text="cliquez")
+result.pack()
 root.bind('<Key>', key_press)
+
 mainloop()
